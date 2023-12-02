3cf9f3520c10538653442c4b8bb27c7f5d79399c
Nguyen Binh <nguyenbinh@Nguyens-MacBook-Pro.local>
2023-12-02 16:22:44 +0700

    make it better

---
+++ main.py
@@ -0,0 +1,11 @@
+# Tạo một tệp tin có tên là app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
