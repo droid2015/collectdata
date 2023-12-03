60c256b044fdde959bf0ee5a1e2f503050360008
Nguyen Binh <nguyenbinh@Nguyens-MacBook-Pro.local>
2023-12-03 12:12:04 +0700

    ok

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 # Tạo một tệp tin có tên là app.py
-from flask import Flask
+from threading import Thread
+import time
+from flask import Flask, render_template
+
+from get_gold import get_gold_price
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+@app.route('/gold_table')
+def gold_table():
+    # Giả sử gold_entries là một danh sách các dữ liệu giá vàng
+    return render_template('gold_table.html', gold_entries=gold_entries)
+def update_gold_data():
+    global gold_entries
+    previous_update_time = None
+    while True:
+        update_time, new_gold_entries = get_gold_price(previous_update_time)
 
+        if update_time and new_gold_entries:
+            previous_update_time = update_time
+            # Cập nhật dữ liệu giá vàng
+            gold_entries = new_gold_entries
+
+        # Chờ 1-2 phút trước khi lấy dữ liệu tiếp theo
+        time.sleep(120)
+# Đặt tiến trình cập nhật dữ liệu giá vàng thành một luồng
+update_thread = Thread(target=update_gold_data)
+# Đặt tiến trình ở chế độ daemon để nó sẽ kết thúc khi chương trình chính kết thúc
+update_thread.daemon = True
+update_thread.start()
 if __name__ == '__main__':
     app.run()
+
+
---
+++ get_gold.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+# Lấy dữ liệu giá vàng từ một API
+def get_gold_price(previous_update_time=None):
+    url = "https://bieudogiavang.vn/"
+    # Gửi yêu cầu GET để tải nội dung trang web
+    response = requests.get(url)
+    # Kiểm tra xem yêu cầu có thành công không (status code 200 là thành công)
+    if response.status_code == 200:
+    # Sử dụng BeautifulSoup để phân tích nội dung HTML
+        soup = BeautifulSoup(response.text, "html.parser")
+        # Tìm thời gian cập nhật
+        thoigian = soup.select_one(".text-amber-500").text.strip().split("lúc")[1].strip()
+        thoigian = thoigian.replace(" ", "")
+        update_time = datetime.strptime(thoigian, "%H:%Mngày%d/%m/%Y")
+        if previous_update_time is None or update_time > previous_update_time:
+            gold_entries = []
+            gold_table = soup.find("table", {"class": "min-w-full divide-y font-base text-sm divide-gray-200 text-gray-800"})
+            rows = gold_table.select("tbody tr")
+            if rows:
+                # In thông tin giá vàng
+                gold_data = []
+                for number, row in enumerate(rows, start=1):
+                    name = row.select_one(".w-64 span").text.strip()
+                    buy_price = row.select(".text-right")[0].select_one(".tabular-nums").text.strip().replace(".", "")
+                    buy_price_change = row.select(".text-right")[0].select_one(".text-red-500")
+                    if buy_price_change:
+                        buy_price_change = buy_price_change.text.strip()
+                    sell_price = row.select(".text-right")[1].select_one(".tabular-nums").text.strip().replace(".", "")
+                    sell_price_change = row.select(".text-right")[1].select_one(".text-red-500")
+                    if sell_price_change:
+                        sell_price_change = sell_price_change.text.strip()
+                    entry = {
+                        'dateupdate':update_time,
+                        'number': number,
+                        'name': name,
+                        'buy_price': buy_price,
+                        'buy_price_change': buy_price_change,
+                        'sell_price': sell_price,
+                        'sell_price_change': sell_price_change
+                    }
+                    gold_entries.append(entry)
+
+                return update_time, gold_entries
+    return None, None
